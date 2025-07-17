c8f0f1c9dc9ecdb5085f7a7cfad17055c23dacfb
Jan Kandrac <jan@kandrac.xyz>
2025-07-17 09:50:02 +0200

    Onboarding implementation

--- app/src/main/java/sk/skillmea/auth/ui/screen/landing.kt
+++ app/src/main/java/sk/skillmea/auth/ui/screen/landing.kt
@@ -1,7 +1,9 @@
 package sk.skillmea.auth.ui.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,11 +11,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -26,6 +34,7 @@ import sk.skillmea.auth.ui.colorBlack
 import sk.skillmea.auth.ui.colorWhite
 import sk.skillmea.auth.ui.textStyleBodyRegular
 import sk.skillmea.auth.ui.widget.SkillmeaButton
+import java.lang.IllegalStateException
 
 @Composable
 fun LandingScreen(
@@ -33,13 +42,32 @@ fun LandingScreen(
     onLogIn: () -> Unit
 ) {
     Box(Modifier.fillMaxSize()) {
-        Image(
-            painter = painterResource(R.drawable.onboarding_1),
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
-        )
+        val state = rememberPagerState { 3 }
+        HorizontalPager(state) { page ->
+            val res = when(page) {
+                0 -> R.drawable.onboarding_1
+                1 -> R.drawable.onboarding_2
+                2 -> R.drawable.onboarding_3
+                else -> throw IllegalStateException()
+            }
+            Image(
+                painter = painterResource(res),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
         Column(Modifier.align(Alignment.BottomCenter).padding(vertical = 32.dp, horizontal = 16.dp).navigationBarsPadding(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Box {
+                Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                    repeat(3) {
+                        Box(Modifier.size(8.dp).background(Color(0xFF909090), CircleShape))
+                    }
+                }
+                Box(Modifier.size(8.dp).offset(
+                    (state.currentPage * 12).dp + (state.currentPageOffsetFraction * 12).dp
+                ).background(Color.White, CircleShape))
+            }
             Text("Welcome to Classroom", color = colorWhite, fontWeight = FontWeight.Black, fontSize = 28.sp, lineHeight = 40.sp, textAlign = TextAlign.Center)
             Spacer(Modifier.height(12.dp))
             Text("Join over 10.000 learners over the World and enjoy online education!", style = textStyleBodyRegular, color = colorWhite, textAlign = TextAlign.Center)
